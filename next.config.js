22f65122ceff8a88881ba9ac29e7b09ee885586f
Kacem AIT OUAL <kacem.aitoual@gmail.com>
2026-03-17 14:35:32 +0100

    Add Next.js configuration with image optimization

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     remotePatterns: [
       {
@@ -9,10 +8,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-  },
-  env: {
-    AEM_ENDPOINT_URL: process.env.AEM_ENDPOINT_URL,
-    AEM_AUTH_TOKEN: process.env.AEM_AUTH_TOKEN,
   },
 };
 
