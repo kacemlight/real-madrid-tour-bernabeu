4e6750e77e46db45580ea7ec8cbd0ad9be104e46
Kacem AIT OUAL <kacem.aitoual@gmail.com>
2026-04-01 14:59:06 +0200

    feat: Add NextJS scaffold with 7 components, AEM integration, and environment config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'publish-*.adobeaemcloud.com' },
-      { protocol: 'https', hostname: 'www.realmadrid.com' },
-    ],
-  },
+      { protocol: 'https', hostname: '**' }
+    ]
+  }
 };
 module.exports = nextConfig;
